0de9dc0252c3f5e46bbdac497682bfc2c3fce0cc
maxime.demouchy@gmail.com <maxime.demouchy@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-16 15:31:38 +0000

    fdfd

--- includes/server.h
+++ includes/server.h
@@ -5,10 +5,12 @@
 ** Login   <demouc_m@epitech.net>
 ** 
 ** Started on  Thu Apr  5 15:58:35 2012 maxime demouchy
-** Last update Mon Apr 16 11:15:13 2012 maxime demouchy
+** Last update Mon Apr 16 16:42:45 2012 maxime demouchy
 */
 
 #ifndef		__SERVER__
 #define		__SERVER__
+
+int	init_server(int port);
 
 #endif
