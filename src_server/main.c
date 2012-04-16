a80a9d62db99739f37bb16cb361b485ae9556650
maxime.demouchy@gmail.com <maxime.demouchy@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-16 14:47:54 +0000

    Server Init

--- src_server/main.c
+++ src_server/main.c
@@ -5,14 +5,51 @@
 ** Login   <demouc_m@epitech.net>
 ** 
 ** Started on  Mon Apr  2 14:13:47 2012 maxime demouchy
-** Last update Mon Apr 16 11:17:53 2012 maxime demouchy
+** Last update Mon Apr 16 16:47:25 2012 maxime demouchy
 */
 
+#include	<sys/socket.h>
+#include	<sys/types.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+#include	<stdio.h>
+#include	<unistd.h>
 #include	"header.h"
 #include	"server.h"
 #include	"common.h"
 
+int	launch_server(int s_server)
+{
+  int			client_len;
+  int			s_client;
+  struct sockaddr_in	sin_client;
+
+  client_len = sizeof(sin_client);
+  while (1)
+    {
+      if ((s_client = accept(s_server,
+			     (struct sockaddr *)&sin_client,
+			     (socklen_t *)&client_len)) == -1)
+	{
+	  close(s_server);
+	  return (-1);
+	}
+      //accept SUCCESS
+    }
+  return (1);
+}
+
 int	main(int argc, char **argv)
 {
+  int	s_server;
+
+  (void)argc;
+  (void)argv;
+  if ((s_server = init_server(45455)) == -1)
+    printf("Erreur initialisation server !\n");
+  else
+    {
+      
+    }
   return (0);
 }
