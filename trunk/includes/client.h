8b0d0034d74427ae9bbdfa55b60cd5e7c1201893
jdourlens@gmail.com <jdourlens@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-18 19:43:32 +0000

    client.h

--- trunk/includes/client.h
+++ trunk/includes/client.h
@@ -5,24 +5,46 @@
 ** Login   <demouc_m@epitech.net>
 ** 
 ** Started on  Sun Apr  8 16:12:39 2012 maxime demouchy
-** Last update Wed Apr 18 18:38:36 2012 jules1 dourlens
+** Last update Wed Apr 18 21:39:11 2012 jules1 dourlens
 */
 
 #ifndef			__CLIENT__
 #define			__CLIENT__
 
 #include		<arpa/inet.h>
+#include		<unistd.h>
 #include		<sys/select.h>
+#include		<ncurses.h>
 
 #define JOIN		"/join"
 #define PART		"/part"
 #define NICK		"/nick"
+
+#define DELIM		" "
+
+typedef struct		s_gui_box
+{
+  int			x;
+  int			y;
+  WINDOW		*win;
+  int			w;
+  int			h;
+}			t_gui_box;
+
+typedef struct		s_event
+{
+  int			type;
+  int			id;
+  struct s_event	*next;
+}			t_event;
 
 typedef struct		s_context
 {
   int			socket;
   struct sockaddr_in	server;
   int			run;
+  fd_set		read;
+  t_gui_box		records;
 }			t_context;
 
 #endif
