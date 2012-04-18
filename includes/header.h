8700da8dbaf2e31a052ef63defe9d72e33b8b020
maxime.demouchy@gmail.com <maxime.demouchy@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-18 16:09:51 +0000

    fdfd

--- includes/header.h
+++ includes/header.h
@@ -5,7 +5,7 @@
 ** Login   <demouc_m@epitech.net>
 ** 
 ** Started on  Mon Apr  2 14:16:50 2012 maxime demouchy
-** Last update Wed Apr 18 14:52:09 2012 maxime demouchy
+** Last update Wed Apr 18 18:09:32 2012 maxime demouchy
 */
 
 #ifndef		__HEADER__
@@ -14,24 +14,31 @@
 #include	<time.h>
 
 #define	LEN_NAME	32
+#define	LEN_DATA	512
+
+#define	PORC		4242
 
 typedef enum
   {
     REGISTER,
+    JOIN_CHAN,
     SEND_MESSAGE,
+    QUIT_CHAN,
+    LIST_CHAN,
+    LIST_USERS,
     SEND_FILE,
     GET_FILE,
-    QUIT_CHAN,
-    LIST_CHAN
+    ANSWER_OK,
+    ANSWER_NO
   }t_type;
 
 typedef struct	s_packet
 {
   t_type	type;
-  char		data[512];
-  char		to[LEN_NAME];
-  char		to_chan[LEN_NAME];
+  unsigned int	id;
   time_t	time;
+  char		data[LEN_DATA];
+  char		to[LEN_NAME];
 }		t_packet;
 
 #endif
