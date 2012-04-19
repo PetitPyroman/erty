0d82211332745b60380071f0c48126dfc8d50715
jdourlens@gmail.com <jdourlens@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-19 16:26:11 +0000

    okokok

--- src_client/event_manager.c
+++ src_client/event_manager.c
@@ -5,13 +5,30 @@
 ** Login   <dourle_a@epitech.net>
 ** 
 ** Started on  Thu Apr 19 12:24:52 2012 jules1 dourlens
-** Last update Thu Apr 19 12:35:18 2012 jules1 dourlens
+** Last update Thu Apr 19 17:19:43 2012 jules1 dourlens
 */
 
+#include		<stdio.h>
 #include		<stdlib.h>
 #include		<string.h>
 #include		"header.h"
 #include		"client.h"
+
+void			event_gere(t_context *c, t_packet *p)
+{
+  t_event		*event;
+
+  printf("Gere: Data : %s , ID : %i \n", p->data, p->id);
+  if (-1 != p->id)
+    {
+      event = get_event_by_id(c, p->id);
+      if (event)
+	{
+	  printf("Event: ID : %i \n", event->id);
+	  event_pop(c, p->id);
+	}
+    }
+}
 
 void			event_push(t_context *c, int c_id, int c_type)
 {
