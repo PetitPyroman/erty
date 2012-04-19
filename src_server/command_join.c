a0e871de72d91285964bc5413959b0e746510102
maxime.demouchy@gmail.com <maxime.demouchy@gmail.com@eec9fb09-f90a-aa8c-3f2d-299851b04a2e>
2012-04-19 15:12:24 +0000

    server

--- src_server/command_join.c
+++ src_server/command_join.c
@@ -5,14 +5,34 @@
 ** Login   <demouc_m@epitech.net>
 ** 
 ** Started on  Wed Apr 18 23:18:31 2012 maxime demouchy
-** Last update Thu Apr 19 16:55:36 2012 maxime demouchy
+** Last update Thu Apr 19 17:10:50 2012 maxime demouchy
 */
 
+#include	<string.h>
 #include	"header.h"
 #include	"server.h"
 
+static t_channel	*get_channel(t_channel *c, char *name)
+{
+  while (c)
+    {
+      if (!(strcmp(c->name, name)))
+	return (c);
+      c = c->next;
+    }
+  return (NULL);
+}
+
+static t_channel	*create_new_channel(t_context *c, char *name)
+{
+  channel_add(&(c->channel), name);
+  return (get_channel(c->channel, name));
+}
+
 void		check_data_join_chan(t_receive *r, t_context *c, t_packet *p)
 {
+  t_channel	*chan;
+
   p->time = time(NULL);
   p->type = ANSWER_NO;
   if (strlen(r->packet.data) < 3 || strlen(r->packet.data) >= LEN_NAME / 2)
@@ -20,7 +40,15 @@ void		check_data_join_chan(t_receive *r, t_context *c, t_packet *p)
   else
     {
       p->type = ANSWER_OK;
-      
+      if ((chan = get_channel(c->channel, r->packet.data)) == NULL)
+	{
+	  strcpy(p->data, "Created new channel !\n");
+	  r->user_from->channel = create_new_channel(c, r->packet.data);
+	}
+      else
+	{
+	  strcpy(p->data, "Join channel !\n");
+	  r->user_from->channel = chan; 
+	}
     }
-  
 }
